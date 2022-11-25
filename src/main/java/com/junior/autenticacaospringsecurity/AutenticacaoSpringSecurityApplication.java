850cb81868b949757b7ad54a291fdb939beec558
JuniorCesar23 <cesarck36@gmail.com>
2022-11-25 08:37:43 -0300

    first commit

---
+++ src/main/java/com/junior/autenticacaospringsecurity/AutenticacaoSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.junior.autenticacaospringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticacaoSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticacaoSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junior/autenticacaospringsecurity/AutenticacaoSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junior.autenticacaospringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticacaoSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
